717d15a0f5805b4018b38adb1cd733216ff55fa5
Joe Cowton <joecowton@gmail.com>
2018-03-14 14:30:37 +0000

    login button

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	compenentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyA-ZsHBLb3NMCgYEjSEdyHzGpUx8etU_io',
+			authDomain: 'auth-ed6bb.firebaseapp.com',
+			databaseURL: 'https://auth-ed6bb.firebaseio.com',
+			projectId: 'auth-ed6bb',
+			storageBucket: 'auth-ed6bb.appspot.com',
+			messagingSenderId: '296036634912'
+		});
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>AN APP</Text>
+				<LoginForm />
 			</View>
 		);
 	}
