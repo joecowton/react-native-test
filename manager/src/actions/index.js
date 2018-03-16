f050f3b9dde41cc4f792397e3b10ea27b19f5ef7
Joe Cowton <joecowton@gmail.com>
2018-03-16 13:20:25 +0000

    firebase login working

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = text => {
 	return {
@@ -15,9 +15,13 @@ export const passwordChanged = text => {
 	};
 };
 
-export const loginUser = ({ user, password }) => {
-	firebase
-		.auth()
-		.signInWithEmailAndPassword(email, password)
-		.then(user => console.log(user));
+export const loginUser = ({ email, password }) => {
+	return dispatch => {
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(email, password)
+			.then(user => {
+				dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+			});
+	};
 };
