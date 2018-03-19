bf277a4097802f374aeea9f7a73423526f5c92a5
Joe Cowton <joecowton@gmail.com>
2018-03-19 11:27:16 +0000

    installs router

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -2,9 +2,11 @@ import {
 	EMAIL_CHANGED,
 	PASSWORD_CHANGED,
 	LOGIN_USER_SUCCESS,
-	LOGIN_USER_FAIL
+	LOGIN_USER_FAIL,
+	LOGIN_USER
 } from '../actions/types';
-const INITIAL_STATE = { email: '', password: '', error: '' };
+
+const INITIAL_STATE = { email: '', password: '', error: '', loading: false };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
@@ -12,10 +14,12 @@ export default (state = INITIAL_STATE, action) => {
 			return { ...state, email: action.payload };
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload };
+		case LOGIN_USER:
+			return { ...state, loading: true, error: '' };
 		case LOGIN_USER_SUCCESS:
-			return { ...state, user: action.payload };
+			return { ...state, ...INITIAL_STATE, user: action.payload };
 		case LOGIN_USER_FAIL:
-			return { ...state, error: 'Authentication Failed.' };
+			return { ...state, error: 'Authentication Failed.', loading: false };
 		default:
 			return state;
 	}
