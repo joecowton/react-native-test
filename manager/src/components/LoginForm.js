94cbc21aba953542e24b5f666329c22da19c70e5
Joe Cowton <joecowton@gmail.com>
2018-03-19 10:48:42 +0000

    authentication error message

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -16,6 +17,16 @@ class LoginForm extends Component {
 		const { email, password } = this.props;
 
 		this.props.loginUser({ email, password });
+	}
+
+	renderError() {
+		if (this.props.error) {
+			return (
+				<View style={{ backgroundColor: 'white' }}>
+					<Text style={styles.errorTextStyle}>{this.props.error}</Text>
+				</View>
+			);
+		}
 	}
 
 	render() {
@@ -38,6 +49,9 @@ class LoginForm extends Component {
 						value={this.props.password}
 					/>
 				</CardSection>
+
+				{this.renderError()}
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>Login</Button>
 				</CardSection>
@@ -46,10 +60,21 @@ class LoginForm extends Component {
 	}
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red',
+		paddingTop: 5,
+		paddingBotton: 5
+	}
+};
+
 const mapStateToProps = state => {
 	return {
 		email: state.auth.email,
-		password: state.auth.password
+		password: state.auth.password,
+		error: state.auth.error
 	};
 };
 
